ac8323194b58c8512cefd888a104ed310e32a5e2
Erum azeemi qaimkhani <160758395+erumQaimkhani@users.noreply.github.com>
2025-01-29 20:13:19 +0500

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import Shop from '../shop/page'
+// import React from 'react'
+// import Shop from '../shop/page'
 
-export default function About() {
+// export default function About() {
+//   return (
+//     <>
+//   <Shop />
+//     </>
+//   )
+// }
+import React from 'react';
+import dynamic from 'next/dynamic';
+
+// Dynamically import the Shop component with SSR disabled
+const Shop = dynamic(() => import('../shop/page'), { ssr: false });
+
+const About: React.FC = () => {
   return (
     <>
-  <Shop />
+      <Shop />
     </>
-  )
+  );
 }
+
+export default About;
